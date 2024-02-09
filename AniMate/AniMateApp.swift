dc435d2e968e52bf250a0a5c57550931002c199c
Sheridan Gomes <me@sheridangomes.com>
2024-02-09 22:48:18 +1100

    Initial Commit

---
+++ AniMate/AniMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AniMateApp.swift
+//  AniMate
+//
+//  Created by Sheridan Gomes on 9/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct AniMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
